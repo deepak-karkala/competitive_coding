ff32d5067d637d7c56c04c779d18f1f651fe191a
deepak-karkala <dkarkala01@gmail.com>
2023-01-09 21:27:26 +0530

    Exponent function

--- leetcode/arrays/MyPow.java
+++ leetcode/arrays/MyPow.java
@@ -1,5 +1,5 @@
-class MyPow {
-    public static double myPow(double x, int n) {
+public class myPow {
+    public double myPow(double x, int n) {
         if (x == 0.0) return 0;
         if (n == 0) return 1;
 
@@ -9,9 +9,5 @@ class MyPow {
         }
 
         return (n%2==0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(myPow(3.0, 2));
     }
 }
