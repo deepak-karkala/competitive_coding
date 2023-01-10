2e862e95822b06dcb5c3dfcf330cf1cc2a64411b
deepak-karkala <dkarkala01@gmail.com>
2023-01-10 09:57:05 +0530

    LCP string - approach 2

--- leetcode/strings/LcpString.java
+++ leetcode/strings/LcpString.java
@@ -1,6 +1,6 @@
 public class LcpString {
     
-    public static String lcpString(String [] strs) {
+    public static String lcpStringSb(String [] strs) {
         StringBuilder sb = new StringBuilder();
         sb.append("");
         for(int i=0; i<strs[0].length(); i++) {
@@ -19,6 +19,21 @@ public class LcpString {
         }
         return sb.toString();
     }
+
+
+    public static String lcpString(String[] strs) {
+        if (strs==null || strs.length==0) return "";
+
+        for(int i=0; i<strs[0].length(); i++) {
+            for(int j=1; j<strs.length; j++) {
+                if ( (i>=strs[j].length()) || (strs[0].charAt(i) != strs[j].charAt(i)) ) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
 
     public static void main(String[] args) {
         String[] strs = {"flower","flow","flight"};
