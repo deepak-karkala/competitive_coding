0804c0f2f6852f35feabdc48e056e667cef43441
deepak-karkala <dkarkala01@gmail.com>
2023-01-19 10:43:32 +0530

    Intersection of arrays: binary search

--- leetcode/two_pointers/Intersection.java
+++ leetcode/two_pointers/Intersection.java
@@ -67,6 +67,50 @@ public class Intersection {
         return result;
     }
 
+    /*
+     * Binary search: Sort first array
+        For each element in second array, do a binary search in first array
+        If it exists, add element to list/set
+        O(nlogn)
+     */
+    public static int[] intersection_binarysearch(int[] nums1, int[] nums2) {
+        HashSet<Integer> intr = new HashSet<Integer>();
+        //ArrayList<Integer> intr = new ArrayList<Integer>();
+
+        Arrays.sort(nums1);
+        for(int i=0; i<nums2.length; i++) {
+            if (binarysearch(nums1, nums2[i])) intr.add(nums2[i]);
+        }
+
+        int[] result = set_to_array(intr);
+        //int[] result = list_to_array(intr);
+        return result;
+    }
+
+    /*
+     * Hashsets: One to store all elements of one array, other to store common elements
+        Iterate through first array and add all elements to hash-set
+        Iterate through second, if it exists in hash-set then add that element to another hash-set
+        O(n) time with O(n) extra space
+     */
+
+    public static boolean binarysearch(int[] arr, int key) {
+        int low = 0, high = arr.length-1;
+        while (low <= high) {
+            int mid = low + ((high-low)/2);
+
+            if (arr[mid] == key) {
+                return true;
+            } else if (arr[mid] > key) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+
     public static int[] set_to_array(HashSet<Integer> hs) {
         int[] res = new int[hs.size()];
         int i = 0;
@@ -88,7 +132,7 @@ public class Intersection {
     public static void main(String[] args) {
         int[] nums1 = {1,2,2,1};
         int[] nums2 = {2,2};
-        int[] intr = intersection_sort(nums1, nums2);
+        int[] intr = intersection_binarysearch(nums1, nums2);
         System.out.println(Arrays.toString(intr));
     }
 }
