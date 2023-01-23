fa589a546aabbd9cbe58b84eff6b72108a6a5995
deepak-karkala <dkarkala01@gmail.com>
2023-01-23 11:05:53 +0530

    Number of 1s - bit shift

--- leetcode/bit_manipulation/HammingWeight.java
+++ leetcode/bit_manipulation/HammingWeight.java
@@ -3,9 +3,15 @@ public class HammingWeight {
     public static int hammingweight(int n) {
         int count = 0;
 
+        /*
         while(n != 0) {
             count += (n & 1);
             n = n>>>1;
+        }
+        */
+
+        for(int i=0; i<32; i++) {
+            count += (n>>i & 1);
         }
         return count;
     }
