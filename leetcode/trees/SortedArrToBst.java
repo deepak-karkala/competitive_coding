22d024e6f0b8e34f3c5f32988098b0abe8c4e559
deepak-karkala <dkarkala01@gmail.com>
2023-03-30 09:25:14 +0530

    145 - Sorted Array to BST - Iterative

--- leetcode/trees/SortedArrToBst.java
+++ leetcode/trees/SortedArrToBst.java
@@ -24,6 +24,7 @@ class TreeNode {
 }
 
 public class SortedArrToBst {
+	// Recursive solution [DFS]
 	public static TreeNode sortedArrayToBST(int[] nums) {
 		return sortedArrayToBSTRecurse(nums, 0, nums.length-1);
     }
@@ -39,6 +40,51 @@ public class SortedArrToBst {
     	return root;
     }
 
+    // Iterative solution [BFS]
+    public static TreeNode sortedArrayToBSTIterative(int[] nums){
+    	if (nums==null || nums.length==0) return null;
+
+    	Queue<Node> queue = new LinkedList<>();
+    	int low = 0, high = nums.length-1;
+    	int mid = low + (high-low)/2;
+    	TreeNode root = new TreeNode(nums[mid]);
+    	queue.offer(new Node(root, low, high));
+
+    	while(!queue.isEmpty()){
+    		//int size = queue.size();
+    		//for(int i=0; i<size; i++){
+    			Node node = queue.poll();
+
+    			//Compute mid for this node
+    			mid = node.low + (node.high-node.low)/2;
+
+    			//If not leaf node, assign left and right subtrees
+    			if (mid != node.low) {
+    				TreeNode left = new TreeNode(nums[node.low + (mid-1-node.low)/2]);
+    				node.node.left = left;
+    				queue.offer(new Node(left, node.low, mid-1));
+    			}
+    			if (mid != node.high) {
+    				TreeNode right = new TreeNode(nums[mid+1 + (node.high-(mid+1))/2]);
+    				node.node.right = right;
+    				queue.offer(new Node(right, mid+1, node.high));
+    			}
+    		//}
+    	}
+    	return root;
+    }
+    private static class Node {
+    	TreeNode node;
+    	int low, high;
+    	Node(TreeNode node, int low, int high){
+    		this.node = node;
+    		this.low = low;
+    		this.high = high;
+    	}
+    }
+
+
+    // For printing tree
     public static List<Integer> inOrderTraversal(TreeNode root) {
 		List<Integer> list = new ArrayList<Integer>();
 		inOrderTraversalRecursive(root, list);
@@ -55,7 +101,7 @@ public class SortedArrToBst {
 
 	public static void main(String[] args){
 		int[] nums = {-10,-3,0,5,9};
-		TreeNode root = sortedArrayToBST(nums);
+		TreeNode root = sortedArrayToBSTIterative(nums);
 		System.out.println(inOrderTraversal(root));
 	}
 }
