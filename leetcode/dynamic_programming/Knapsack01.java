259d7c424f0b0caf07016266d006247a60ee8b54
deepak-karkala <dkarkala01@gmail.com>
2023-07-13 13:50:47 +0530

    Knapsack 0-1 - DP Top down with memo

--- leetcode/dynamic_programming/Knapsack01.java
+++ leetcode/dynamic_programming/Knapsack01.java
@@ -32,11 +32,43 @@ class Knapsack01 {
 		return knapsack_recursive_bf(v, w, W, n-1);
 	}
 
+	// Method 2
+	// DP: Top down memoization
+	// There are a total of N * W unique subproblems to be solved
+	// => Time: O(N * W), Space: O(N * W)
+	private static int knapsack_dp_memo_top(int[] v, int[] w, int W) {
+		int n = v.length-1;
+		int[][] memo = new int[n+1][W+1];
+		for(int i=0; i<=n; i++){
+			for(int j=0; j<=W; j++){
+				memo[i][j] = -1;
+			}
+		}
+		return knapsack_dp_memo(v, w, W, n, memo);
+	}
+
+	private static int knapsack_dp_memo(int[] v, int[] w, int W, int n, int[][] memo) {
+		// Base Case
+		if (n<0 || W==0) return 0;
+
+		// Return memoized value if computed previously
+		if (memo[n][W] != -1) return memo[n][W];
+
+		// If current item's weight is less than weight remaining,
+		if (w[n] < W) {
+			return memo[n][W] = Math.max(v[n] + knapsack_dp_memo(v, w, W-w[n], n-1, memo), knapsack_dp_memo(v, w, W, n-1, memo));
+		}
+
+		// If current item's weight is more than weight remaining, this item cannot be picked,
+		return memo[n][W] = knapsack_dp_memo(v, w, W, n-1, memo);
+	}
+
+
 	public static void main(String[] args) {
-		int[] v = {10, 20, 30, 50};
+		int[] v = {10, 20, 30, 40};
 		int[] w = {30, 10, 40, 20};
-		int W = 60;
-		int max_value = knapsack_recursive_bf_top(v, w, W);
+		int W = 40;
+		int max_value = knapsack_dp_memo_top(v, w, W);
 		System.out.println(max_value);
 	}
 }
