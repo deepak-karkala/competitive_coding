e6dced7d009540d3336094a5fcf2695e562b0dd4
deepak-karkala <dkarkala01@gmail.com>
2023-07-13 14:28:07 +0530

    Knapsack 0-1 - DP Bottom up iterative

--- leetcode/dynamic_programming/Knapsack01.java
+++ leetcode/dynamic_programming/Knapsack01.java
@@ -33,7 +33,7 @@ class Knapsack01 {
 	}
 
 	// Method 2
-	// DP: Top down memoization
+	// DP: Top down memoization (out of 2^n total subproblems, many are overlapping)
 	// There are a total of N * W unique subproblems to be solved
 	// => Time: O(N * W), Space: O(N * W)
 	private static int knapsack_dp_memo_top(int[] v, int[] w, int W) {
@@ -63,12 +63,41 @@ class Knapsack01 {
 		return memo[n][W] = knapsack_dp_memo(v, w, W, n-1, memo);
 	}
 
+	// Method 3
+	// DP: Bottom up iterative
+	// There are a total of N * W unique subproblems to be solved
+	// => Time: O(N * W), Space: O(N * W)
+	private static int knapsack_dp_iterative(int[] v, int[] w, int W) {
+		int n = v.length;
+		int[][] memo = new int[n+1][W+1];
+
+		// If W==0, (weight entirely used up) then for all n, memo[n][0] = 0
+		for(int i=0; i<=n; i++) memo[i][0] = 0;
+		// If n==0, (no items left to be picked), then for all W, memo[0][W] = 0
+		for(int j=0; j<=W; j++) memo[0][j] = 0;
+
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=W; j++) {
+
+				// If current item's weight is less than remaining weight, 2 options,
+				// Either pick current item or ignore. Use max of these 2 options
+				if (w[i-1] <= j) {
+					memo[i][j] = Math.max(v[i-1] + memo[i-1][j-w[i-1]], memo[i-1][j]);
+				} else {
+				// If current item's weight is more than remaining weight, cannot choose
+				// this item => optimal value is same as with n-1 items with W weight
+					memo[i][j] = memo[i-1][j];	
+				}
+			}
+		}
+		return memo[n][W];
+	}
 
 	public static void main(String[] args) {
 		int[] v = {10, 20, 30, 40};
 		int[] w = {30, 10, 40, 20};
 		int W = 40;
-		int max_value = knapsack_dp_memo_top(v, w, W);
+		int max_value = knapsack_dp_iterative(v, w, W);
 		System.out.println(max_value);
 	}
 }
