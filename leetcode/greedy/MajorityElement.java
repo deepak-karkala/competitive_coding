c3d72ddb2a34a7935066d8e5de6671da0095659e
deepak-karkala <dkarkala01@gmail.com>
2023-08-08 14:06:13 +0530

    169 - Majority element - bit mainpulation

--- leetcode/greedy/MajorityElement.java
+++ leetcode/greedy/MajorityElement.java
@@ -66,9 +66,34 @@ class MajorityElement {
 		return me;
 	}
 
+	// Bit manipulation
+	// Given majority element is present more than len/2 times
+	// => Bit representation of majority element will have number of times 
+	// bits set in those position greater than len/2 times
+	// => Count number of times each bit (in total 32 bits) gets set as you iterate through the array
+	// Time: O(n) Space: O(1)
+	private static int majorityElement_bit(int[] nums) {
+		int[] bits = new int[32];
+
+		// Count number of times bit is set at each position
+		for(int i=0; i<nums.length; i++) {
+			for(int j=0; j<32; j++) {
+				bits[j] += (nums[i]>>j) & 1;
+			}
+		}
+
+		int me = 0;
+		// Determine which bits are set more than len/2 number of times
+		for(int j=0; j<32; j++) {
+			bits[j] = bits[j] > nums.length/2 ? 1 : 0;
+			me += bits[j] << j;
+		}
+		return me;
+	}
+
 	public static void main(String[] args) {
 		int[] nums = {2,2,1,1,1,2,2};
-		int me = majorityElement_voting(nums);
+		int me = majorityElement_bit(nums);
 		System.out.println(me);
 	}
 }
