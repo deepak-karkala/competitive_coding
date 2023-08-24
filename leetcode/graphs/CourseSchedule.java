d11f9552c8fc6c0e7805469a673a10450a95e77e
deepak-karkala <dkarkala01@gmail.com>
2023-08-24 12:48:47 +0530

    207 - Course Schedule - DFS topological sort

--- leetcode/graphs/CourseSchedule.java
+++ leetcode/graphs/CourseSchedule.java
@@ -18,9 +18,10 @@ import java.util.*;
 class CourseSchedule {
 	// BFS Topological Sort
 	// Keep adding vertices with indegree=0 to queue and check if at the end
-	//		all the vertices are visited
+	//		all the vertices are visited. If there are cycles (dependencies),
+	//		topological sort not possible.
 	// Time: O(V + E)
-    private static boolean canFinish(int numCourses, int[][] prerequisites) {
+    private static boolean canFinish_bfs(int numCourses, int[][] prerequisites) {
         // Create adjacency list based on prerequisites and update indegrees of vertices
         int[] indegree = new int[numCourses];
         List<Integer>[] adj = new ArrayList[numCourses];
@@ -55,9 +56,58 @@ class CourseSchedule {
         return numCoursesCompleted == numCourses;
     }
 
+    // DFS Topological Sort
+	// Do DFS, mark vertices as unvisited, being visited, finished visiting
+	//		If at any point, we reach an already finished visited or being visited node
+	//		then it means there is a cycle, return false.
+	//		If not, return true
+	// Time: O(V + E)
+    private static boolean canFinish_dfs(int numCourses, int[][] prerequisites) {
+    	// Create adjacency list based on prerequisites and update indegrees of vertices
+        List<Integer>[] adj = new ArrayList[numCourses];
+        for(int i=0; i<numCourses; i++) adj[i] = new ArrayList<Integer>();
+        for(int i=0; i<prerequisites.length; i++) {
+        	adj[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+
+        // Track 3 states (unvisited:0, being visited:1, finished visiting:2)
+        int[] visited = new int[numCourses];
+        for(int i=0; i<numCourses; i++) visited[i] = 0;
+
+        for(int i=0; i<numCourses; i++) {
+        	// If at any point, we reach an already finished visited or being visited node
+			//		then it means there is a cycle, return false.
+        	if (!dfs(adj, visited, i)) {
+        		return false;
+        	}
+        }
+        return true;
+    }
+
+    private static boolean dfs(List<Integer>[] adj, int[] visited, int course) {
+    	// Return true if this node is already visited
+    	if (visited[course] == 2) return true;
+    	// Return false if this node is being visited (=> there is a cycle)
+    	if (visited[course] == 1) return false;
+
+    	// Set status of this vertex to as being visited
+    	visited[course] = 1;
+
+    	// Explore all the edges from this course
+    	for(int v: adj[course]) {
+    		// Found a cycle (already visited vertex), return false
+    		if (visited[v] == 1) return false;
+    		if (!dfs(adj, visited, v)) return false;
+    	}
+
+    	// Set status of this vertex to as finished visited
+    	visited[course] = 2;
+    	return true;
+    }
+
     public static void main(String[] args) {
     	int numCourses = 2;
-    	int[][] prerequisites = { {1,0}, {0,1} };
-    	System.out.println(canFinish(numCourses, prerequisites));
+    	int[][] prerequisites = { {1,0} };
+    	System.out.println(canFinish_dfs(numCourses, prerequisites));
     }
 }
