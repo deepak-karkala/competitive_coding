ea19e8e9168adc759507b47bd3cc53be438c04c6
deepak-karkala <dkarkala01@gmail.com>
2023-09-11 13:13:30 +0530

    886 - Possible bipartite graph - BFS

--- leetcode/graphs/PossibleBipartition.java
+++ leetcode/graphs/PossibleBipartition.java
@@ -16,6 +16,8 @@ is possible to split everyone into two groups in this way.
 import java.util.*;
 
 class PossibleBipartition {
+	// Graph coloring: DFS
+	// Time: O(V+E) Space: O(V+E)
     private static boolean possibleBipartition_dfs(int n, int[][] dislikes) {
     	// Create adj list, undirected graph
         List<Integer>[] adj = new ArrayList[n];
@@ -44,10 +46,47 @@ class PossibleBipartition {
     	return true;
     }
 
+
+    // Graph coloring: BFS
+	// Time: O(V+E) Space: O(V+E)
+    private static boolean possibleBipartition_bfs(int n, int[][] dislikes) {
+    	// Create adj list, undirected graph
+        List<Integer>[] adj = new ArrayList[n];
+        for(int i=0; i<n; i++) adj[i] = new ArrayList<Integer>();
+        for(int i=0; i<dislikes.length; i++) {
+        	adj[dislikes[i][0]-1].add(dislikes[i][1]-1);
+        	adj[dislikes[i][1]-1].add(dislikes[i][0]-1);
+        }
+
+        // Color 0:unvisited 1:Group1 -1:Group2
+        int[] colors = new int[n];
+
+        // Start BFS for each connected component
+        for(int i=0; i<n; i++) {
+        	if (colors[i] == 0) {
+		        Queue<Integer> queue = new LinkedList<Integer>();
+		        queue.offer(i);
+		        colors[i] = 1;
+
+		        while(!queue.isEmpty()) {
+		        	int node = queue.poll();
+		        	for(int neighbour: adj[node]) {
+		        		if (colors[neighbour] == colors[node]) return false;
+		        		if (colors[neighbour] == 0) {
+		        			colors[neighbour] = -colors[node];
+		        			queue.offer(neighbour);
+		        		}
+		        	}
+		        }
+		    }
+	    }
+        return true;
+    }
+
     public static void main(String[] args) {
-    	int n = 3;
-    	int[][] dislikes = { {1,2}, {1,3}, {2,3} };
-    	boolean possible = possibleBipartition_dfs(n, dislikes);
+    	int n = 5;
+    	int[][] dislikes = { {1,2}, {3,4}, {4,5}, {3,5} };
+    	boolean possible = possibleBipartition_bfs(n, dislikes);
     	System.out.println(possible);
     }
 }
