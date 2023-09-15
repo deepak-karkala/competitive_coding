7cdf435c635746734fe10c8779f07184e038f40d
deepak-karkala <dkarkala01@gmail.com>
2023-09-15 12:32:43 +0530

    542 - Matrix01 - DP

--- leetcode/graphs/Matrix01.java
+++ leetcode/graphs/Matrix01.java
@@ -21,7 +21,7 @@ class Matrix01 {
 		4. 1s with only other 1s as neighbours will then get updated with dist = dist_of_neighbour1 + 1
 	Time: O(m*n) Space:O(m*n)
 	*/
-    private static int[][] updateMatrix(int[][] mat) {
+    private static int[][] updateMatrix_bfs(int[][] mat) {
     	int m=mat.length, n=mat[0].length;
     	Queue<int []> queue = new LinkedList<int []>();
 
@@ -91,9 +91,44 @@ class Matrix01 {
     }
     */
 
+
+    /*
+    Approach DP
+    	Compute distance from two directions
+    		1. Top, left neighbours
+    		2. Bottom, right neighbours
+    	Use min of these two to compute final shortest distances
+    */
+    private static int[][] updateMatrix_dp(int[][] mat) {
+    	int m=mat.length, n=mat[0].length;
+
+    	// Distances from top, left neighbours
+    	for(int i=0; i<m; i++) {
+    		for(int j=0; j<n; j++) {
+    			if (mat[i][j] == 0) continue;
+    			int dist_top = m+n, dist_left = m+n;	//Since m+n is max or can set to MAX_VALUE
+    			if (i>=1) dist_left = mat[i-1][j];
+    			if (j>=1) dist_top = mat[i][j-1];
+    			mat[i][j] = Math.min(dist_left, dist_top) + 1;
+    		}
+    	}
+
+    	// Distances from bottom, right neighbours
+    	for(int i=m-1; i>=0; i--) {
+    		for(int j=n-1; j>=0; j--) {
+    			if (mat[i][j] == 0) continue;
+    			int dist_bottom = m+n, dist_right = m+n;	//Since m+n is max or can set to MAX_VALUE
+    			if (i<=m-2) dist_right = mat[i+1][j];
+    			if (j<=n-2) dist_bottom = mat[i][j+1];
+    			mat[i][j] = Math.min(mat[i][j], Math.min(dist_right, dist_bottom)+1);
+    		}
+    	}
+    	return mat;
+    }
+
     public static void main(String[] args) {
     	int[][] mat = { {0,0,0}, {0,1,0}, {1,1,1} };
-    	mat = updateMatrix(mat);
+    	mat = updateMatrix_dp(mat);
     	for(int i=0; i<mat.length; i++) {
     		for(int j=0; j<mat[0].length; j++) {
     			System.out.print(mat[i][j] + " ");
