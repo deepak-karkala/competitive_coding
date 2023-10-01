bacf28209d6e6b9cff3b2120001059863adcc5d6
deepak-karkala <dkarkala01@gmail.com>
2023-10-01 16:20:03 +0530

    1334 - Find the city - Bellman Ford shortest path

--- leetcode/graphs/shortest_path/FindTheCity.java
+++ leetcode/graphs/shortest_path/FindTheCity.java
@@ -26,28 +26,22 @@ class FindTheCity {
     private static int findTheCity(int n, int[][] edges, int distanceThreshold) {
         // 2D array to store distances from each node to every other node
         int[][] dist = new int[n][n];
-
         // Init distance with max value
         for(int i=0; i<n; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
         for(int i=0; i<n; i++) dist[i][i] = 0;
+
         // Build graph (update weights with given edges)
         for(int[] edge: edges) {
         	dist[edge[0]][edge[1]] = edge[2];
         	dist[edge[1]][edge[0]] = edge[2];
         }
 
-        // Compute distances for every pair of nodes through each intermediate node
-        for(int k=0; k<n; k++)
-        	for(int i=0; i<n; i++) 
-        		for(int j=0; j<n; j++) {
-        			if (dist[i][k]!=Integer.MAX_VALUE && dist[k][j]!=Integer.MAX_VALUE &&
-        				 dist[i][j] > dist[i][k] + dist[k][j]) {
-        				dist[i][j] = dist[i][k] + dist[k][j];
-        			}
-        		}
-
-        //for(int i=0; i<n; i++)
-        //	for(int j=0; j<n; j++) System.out.println(i + "-" + j + ":" + dist[i][j]);
+        //floyd_warshall(n, dist);
+        // Run BFS (Dijkstra / BellmanFord / SPFA from each node)
+        for(int src=0; src<n; src++) {
+            // dijkstra(n, dist[src], adj, src);
+            bellman_ford(n, dist[src], edges, src);
+        }
 
         // For each city, count number of cities below threshold distance
         int[] count = new int[n];
@@ -66,6 +60,58 @@ class FindTheCity {
         }
 
         return min_idx;
+    }
+
+
+    private static void floyd_warshall(int n, int[][] dist) {
+        // Compute distances for every pair of nodes through each intermediate node
+        for(int k=0; k<n; k++)
+            for(int i=0; i<n; i++) 
+                for(int j=0; j<n; j++) {
+                    if (dist[i][k]!=Integer.MAX_VALUE && dist[k][j]!=Integer.MAX_VALUE &&
+                         dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+        return;
+    }
+
+    /*
+	Approach: BFS (Dijkstra's)
+		1. Run BFS from each node
+		2. Count number of cities with distance to other cities less than threshold
+		3. Find the city with least such cities
+    private static void dijkstra(int n, int[][] dist, int adj, int src) {
+    	Queue<Integer> queue = new LinkedList<>();
+    	queue.offer(u);
+
+    	while(!queue.isEmpty()) {
+    		int node = queue.poll();
+
+    		for(int v=0; v<dist.length; v++) {
+    			if (dist[v] > dist[])
+    		}
+    	}
+    }
+    */
+
+    /*
+    Approach: BFS (Bellman Ford)
+        1. Run BFS from each node
+        2. Count number of cities with distance to other cities less than threshold
+        3. Find the city with least such cities
+    */
+    private static void bellman_ford(int n, int[] dist, int[][] edges, int src) {
+        // Run |V| times all edges
+        for(int k=1; k<n; k++) {
+            for(int[] edge: edges) {
+                int u = edge[0], v = edge[1], uv = edge[2];
+                if (dist[v]!=Integer.MAX_VALUE && dist[u] > dist[v] + uv)
+                    dist[u] = dist[v] + uv;
+                if (dist[u]!=Integer.MAX_VALUE && dist[v] > dist[u] + uv)
+                    dist[v] = dist[u] + uv;
+            }
+        }
     }
 
     public static void main(String[] args) {
