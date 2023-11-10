15f0ee36d280517d0b0c521d853199053ff051ec
deepak-karkala <dkarkala01@gmail.com>
2023-11-10 16:25:41 +0530

    76 - Min Window Substring - hashing

--- leetcode/hashing/MinWindowSubstring.java
+++ leetcode/hashing/MinWindowSubstring.java
@@ -50,8 +50,46 @@ class MinWindowSubstring {
         return minLen==Integer.MAX_VALUE ? "" : s.substring(minStart, minStart+minLen);
     }
 
+
+    private static String minWindowSubstring2(String s, String t) {
+        if (s==null || s.length()==0 || s.length()<t.length()) return "";
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE, counter = 0, minLenStart = 0;
+        int[] map = new int[128];
+
+        // Map characters in "t" to number of occurrences
+        for(char c: t.toCharArray()) map[c]++;
+
+        while(end < s.length()) {
+            char c1 = s.charAt(end);
+            // Check if current character occurred in "t"
+            if (map[c1] > 0) counter++;
+            // all character in "t" will be mapped back to 0 when they are seen in "s"
+            map[c1]--;
+            end++;
+
+
+            // As long as all characters in "t" are present in window
+            //      keep moving the start to the left to find min window
+            while(counter == t.length()) {
+                // Update min window length if current window is shorter
+                if (minLen > end - start) {
+                    minLen = end - start;
+                    minLenStart = start; 
+                }
+
+                char c2 = s.charAt(start);
+                map[c2]++;
+                // If this character is in "t", update counter
+                if (map[c2] > 0) counter--;
+                start++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLenStart, minLenStart + minLen);
+    }
+
     public static void main(String[] args) {
     	String s = "ADOBECODEBANC", t = "ABC";
-    	System.out.println(minWindowSubstring(s, t));
+    	System.out.println(minWindowSubstring2(s, t));
     }
 }
