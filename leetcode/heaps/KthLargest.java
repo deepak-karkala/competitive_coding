382f94411847ac44f275be7d8e537f353f65475f
deepak-karkala <dkarkala01@gmail.com>
2023-11-19 12:58:27 +0530

    215 - Kth largest - min heap

--- leetcode/heaps/KthLargest.java
+++ leetcode/heaps/KthLargest.java
@@ -12,7 +12,11 @@ Can you solve it without sorting?
 import java.util.*;
 
 class KthLargest {
-    private static int findKthLargest(int[] nums, int k) {
+    /* 
+        Max Heap
+        Time: O(k * logn) Space: O(n)
+    */
+    private static int findKthLargest_maxHeap(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         for(int n: nums) pq.offer(n);
 
@@ -23,9 +27,24 @@ class KthLargest {
         return kthLargest;
     }
 
+    /* 
+        Min Heap
+        Time: O(n * logk) Space: O(k)
+    */
+    private static int findKthLargest_minHeap(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int n: nums) {
+            pq.offer(n);
+
+            if (pq.size() > k) pq.poll();
+        }
+
+        return pq.peek();
+    }
+
     public static void main(String[] args) {
     	int[] nums = {3,2,3,1,2,4,5,5,6};
     	int k = 4;
-    	System.out.println(findKthLargest(nums, k));
+    	System.out.println(findKthLargest_minHeap(nums, k));
     }
 }
