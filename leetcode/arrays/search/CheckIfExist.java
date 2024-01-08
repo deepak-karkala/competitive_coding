1beb1b780a19e568b3965ae679e1b553d71202b9
deepak-karkala <dkarkala01@gmail.com>
2024-01-08 18:55:27 +0530

    1346 - Check if double exists - hashset

---
+++ leetcode/arrays/search/CheckIfExist.java
@@ -0,0 +1,31 @@
+/*
+1346. Check If N and Its Double Exist
+Easy
+Topics
+Companies
+Hint
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+*/
+
+import java.util.*;
+
+class CheckIfExist {
+    private static boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for(int n: arr) {
+        	if (set.contains(n*2)) return true;
+            if (n%2==0 && set.contains(n/2)) return true;
+        	set.add(n);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+    	int[] arr = {10,2,5,3};
+    	System.out.println(checkIfExist(arr));
+    }
+}
