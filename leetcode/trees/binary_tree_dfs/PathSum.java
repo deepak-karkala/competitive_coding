7a3ec404ee14b6c2e8ac2d41325ecf32bac01bab
deepak-karkala <dkarkala01@gmail.com>
2024-01-30 22:50:22 +0530

    112 - Path Sum - DFS

---
+++ leetcode/trees/binary_tree_dfs/PathSum.java
@@ -0,0 +1,31 @@
+/*
+112. Path Sum
+Solved
+Easy
+Topics
+Companies
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+*/
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode() {}
+	TreeNode(int val) { this.val = val; }
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+class PathSum {
+    private static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && targetSum == root.val) return true;
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
