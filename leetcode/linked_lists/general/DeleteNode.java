95d9593a377dc58c8bded36a4160e34da3c7edfa
deepak-karkala <dkarkala01@gmail.com>
2024-03-01 19:19:31 +0530

    237 - Delete node - overwrite with next val

---
+++ leetcode/linked_lists/general/DeleteNode.java
@@ -0,0 +1,18 @@
+/*
+237. Delete Node in a Linked List
+Medium
+Topics
+Companies
+There is a singly-linked list head and we want to delete a node node in it.
+
+You are given the node to be deleted node. You will not be given access to the first node of head.
+
+All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+*/
+
+class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
