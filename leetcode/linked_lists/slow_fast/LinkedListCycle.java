603b659371067fd16daa1c4d63bbb6964f4f3bc9
deepak-karkala <dkarkala01@gmail.com>
2024-03-15 19:46:52 +0530

    141 - Linked list cycle - slow fast

---
+++ leetcode/linked_lists/slow_fast/LinkedListCycle.java
@@ -0,0 +1,35 @@
+/*
+141. Linked List Cycle
+Solved
+Easy
+Topics
+Companies
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+*/
+
+class ListNode {
+    int val;
+    ListNode next, prev;
+    ListNode() {};
+    ListNode(int val) {this.val = val;}
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+}
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
