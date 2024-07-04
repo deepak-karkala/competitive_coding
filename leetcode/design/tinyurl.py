7d725a687b12b404867fb6409bb154fbf10e7279
deepak-karkala <dkarkala01@gmail.com>
2024-07-04 20:12:34 +0530

    535 - Tiny URL - hash

---
+++ leetcode/design/tinyurl.py
@@ -0,0 +1,50 @@
+"""
+535. Encode and Decode TinyURL
+Solved
+Medium
+Topics
+Companies
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk. Design a class to encode a URL and decode a tiny URL.
+
+There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+Implement the Solution class:
+
+Solution() Initializes the object of the system.
+String encode(String longUrl) Returns a tiny URL for the given longUrl.
+String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
+"""
+
+class Codec:
+
+    alphabet = string.ascii_letters + '0123456789'
+
+    def __init__(self):
+        self.url2code = {}
+        self.code2url = {}
+
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+        
+        :type longUrl: str
+        :rtype: str
+        """
+        while longUrl not in self.url2code:
+            code = ''.join(random.choice(Codec.alphabet) for _ in range(6))
+            if code not in self.code2url:
+                self.url2code[longUrl] = code
+                self.code2url[code] = longUrl
+        return 'http://tinyurl.com/' + self.url2code[longUrl]
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.code2url[shortUrl[-6:]]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
