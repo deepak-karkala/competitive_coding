0e6e00f7eb1dba2a239509d8b84c9381f923d686
deepak-karkala <dkarkala01@gmail.com>
2024-07-09 10:18:44 +0530

    lsof -i:9001 - Longest consecutive sequence - uf, hash

---
+++ leetcode/graphs/disjoint_set/longest_consecutive.py
@@ -0,0 +1,28 @@
+'''
+128. Longest Consecutive Sequence
+Solved
+Medium
+Topics
+Companies
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+'''
+
+class longest_consecutive(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
+
