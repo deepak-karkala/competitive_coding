e0fb47f616f1427f19354ed4a67b34d836ab6815
deepak-karkala <dkarkala01@gmail.com>
2024-07-16 20:27:53 +0530

    344 - Reverse string

---
+++ leetcode/strings/reverse_string.py
@@ -0,0 +1,25 @@
+"""
+344. Reverse String
+Solved
+Easy
+Topics
+Companies
+Hint
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+"""
+
+
+class ReverseString(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
