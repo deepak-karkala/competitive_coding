fbb45e416005207fbf9d81714e25b9a49e9ca28c
deepak-karkala <dkarkala01@gmail.com>
2024-08-06 19:58:49 +0530

    167 - two sum sorted- two pointers

---
+++ leetcode/two_pointers/straightforward/two_sum_sorted.py
@@ -0,0 +1,27 @@
+"""
+167. Two Sum II - Input Array Is Sorted
+Solved
+Medium
+Topics
+Companies
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+"""
+
+
+class TwoSumSorted:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else:
+                left += 1
