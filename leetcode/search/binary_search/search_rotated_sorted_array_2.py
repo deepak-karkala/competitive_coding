2a436e60764abec5d4c91b2bbaf1e4dc3cfd82d8
deepak-karkala <dkarkala01@gmail.com>
2024-09-10 19:32:13 +0530

    81 - Search in rotated array 2

---
+++ leetcode/search/binary_search/search_rotated_sorted_array_2.py
@@ -0,0 +1,47 @@
+"""
+81. Search in Rotated Sorted Array II
+Solved
+Medium
+Topics
+Companies
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+"""
+
+
+class SearchRotatedSortedArrayII:
+    def search(self, nums: List[int], target: int) -> bool:
+        """
+        Non distinct values =>
+        nums[low] can be equal to nums[mid]
+        => Not possible to determine which half of array is sorted
+        => Use linear search in this case, incrementing left by one.
+        For other cases, same as search in rotated array with distinct values
+        """
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return True
+
+            if nums[left] < nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[left] > nums[mid]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                left += 1
+
+        return False
